e946d2505facfb5f05b8b120eca67ebf0e228c06
Yoali SB <sb.yoali@gmail.com>
2017-04-24 11:44:49 -0500

    Uploaded existing files to repository

---
+++ POJOs/Terapeuta.java
@@ -0,0 +1,18 @@
+
+public class Terapeuta {
+	private int IDTerapeuta;
+	private String NombreTerapeuta;
+	
+	public int getIDTerapeuta() {
+		return IDTerapeuta;
+	}
+	public void setIDTerapeuta(int iDTerapeuta) {
+		IDTerapeuta = iDTerapeuta;
+	}
+	public String getNombreTerapeuta() {
+		return NombreTerapeuta;
+	}
+	public void setNombreTerapeuta(String nombreTerapeuta) {
+		NombreTerapeuta = nombreTerapeuta;
+	}
+}
